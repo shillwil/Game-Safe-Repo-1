d12f7a37b717ddf53acaec741b444973e602ca79
Alex Shillingford <alex.shillingford@icloud.com>
2019-11-18 23:24:10 -0700

    Create FirstOnboardViewController.swift file for future animations in first onboarding screen

---
+++ The Game Safe/The Game Safe/FirstOnboardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstOnboardViewController.swift
+//  The Game Safe
+//
+//  Created by Alex Shillingford on 11/18/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class FirstOnboardViewController: UIViewController {
+
+    @IBOutlet weak var welcomeStackView: UIStackView!
+    @IBOutlet weak var topTextView: UITextView!
+    @IBOutlet weak var middleTextView: UITextView!
+    @IBOutlet weak var bottomTextView: UITextView!
+    @IBOutlet weak var pointerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
