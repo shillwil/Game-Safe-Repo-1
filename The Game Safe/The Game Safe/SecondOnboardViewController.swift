82d0e2f95e1783865b444c75ef09e0b21523733e
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-11 16:40:44 -0700

    Add animations for SecondOnboardViewController as to ease the load of information shown to the user

--- The Game Safe/The Game Safe/SecondOnboardViewController.swift
+++ The Game Safe/The Game Safe/SecondOnboardViewController.swift
@@ -20,13 +20,74 @@ class SecondOnboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if hasAnimated == false {
+            resetAnimations()
+        }
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if hasAnimated == false {
+            launchOnboardAnimations()
+            hasAnimated = true
+        }
+    }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if hasAnimated == false {
+            resetAnimations()
+        }
+    }
     
+    func launchOnboardAnimations() {
+        UIView.animateKeyframes(withDuration: 6, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.08) {
+                self.topTextView.center.y += 50
+                self.topTextView.alpha = 1
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.08, relativeDuration: 0.08) {
+                self.segmentControlImageView.center.y += 50
+                self.segmentControlImageView.alpha = 1
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.16, relativeDuration: 0.08) {
+                self.easyTextView.center.y += 50
+                self.easyTextView.alpha = 1
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.08) {
+                self.mediumTextView.center.y += 50
+                self.mediumTextView.alpha = 1
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.08) {
+                self.hardTextView.center.y += 50
+                self.hardTextView.alpha = 1
+            }
 
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.08) {
+                self.disclaimerTextView.center.y += 25
+                self.disclaimerTextView.alpha = 1
+            }
+        }, completion: nil)
+    }
+    
+    func resetAnimations() {
+        self.topTextView.center.y -= 50
+        self.topTextView.alpha = 0
+        self.segmentControlImageView.center.y -= 50
+        self.segmentControlImageView.alpha = 0
+        self.easyTextView.center.y -= 50
+        self.easyTextView.alpha = 0
+        self.mediumTextView.center.y -= 50
+        self.mediumTextView.alpha = 0
+        self.hardTextView.center.y -= 50
+        self.hardTextView.alpha = 0
+        self.disclaimerTextView.center.y -= 50
+        self.disclaimerTextView.alpha = 0
+    }
     /*
     // MARK: - Navigation
 
