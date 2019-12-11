d61b237f6a478dc4eed36998d501529cead48cff
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-11 16:43:15 -0700

    Create Onboarding Resources file for organization; create a ThirdOnboardViewController.swift Cocoa Touch Class File.

---
+++ The Game Safe/The Game Safe/Onboarding Resources/ThirdOnboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdOnboardViewController.swift
+//  The Game Safe
+//
+//  Created by Alex Shillingford on 12/11/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ThirdOnboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
