2d22d28620b3fe2200f6eb135d6ae3d899addf66
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-11 22:04:32 -0700

    Connect FourthOnboardViewController scene to FourthOnboardViewController.swift; wire up "Let's Go!" button and have it dismiss the onboard screens

---
+++ The Game Safe/The Game Safe/Onboarding Resources/FourthOnboardViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FourthOnboardViewController.swift
+//  The Game Safe
+//
+//  Created by Alex Shillingford on 12/11/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class FourthOnboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func letsGoButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
